5a8d2f037a5d821ad09afa6e4eaef67961c38a18
slyfox <slyfox2046@gmail.com>
2019-10-11 17:22:28 +0800

    webpack4 study

---
+++ lesson3-4/src/index.js
@@ -0,0 +1,10 @@
+import avatar from  './avatar.jpg';
+import './index.css';
+
+
+var img = new Image();
+img.src =avatar;
+img.classList.add('avatar');//avatar是class名称
+
+var root = document.getElementById("root");
+root.append(img);
---
+++ lesson3-4/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",// development 开发模式，未被压缩，production 线上模式，被压缩了
+
+    // entry :"./src/index.js", //入口文件，放在了src目录下
+    entry :{
+        main:"./src/index.js"
+    },
+    module:{
+        rules:[{
+            test:/\.(jpg|png|gif)$/, //正则，以.jpg结尾的文件
+            use:{
+                // loader:'file-loader',
+                loader:'url-loader',//把图片打包成base64的字符串
+                options : {
+                    // placehoder 占位符
+                    // name:'[name].[ext]',//name 指原来的文件名，ext 文件后缀
+                    name:'[name]_[hash].[ext]',//name 指原来的文件名，ext 文件后缀
+                    outputPath : 'images/' ,  // 将图片打包至images目录下
+                    limit : 2048     // >2kb打包成文件，否则打包成base64的字符串
+                }
+            }
+        },{
+            test:/\.css$/, //正则，以.jpg结尾的文件
+            use:['style-loader','css-loader']
+        }
+
+        ]
+
+    },
+    output : {
+        filename : 'bundle.js', //输出的文件名
+        path : path.resolve(__dirname,'dist')  //配置路径
+    }
+}
