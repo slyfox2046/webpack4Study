5a3cb3735ea95430cd333205fa625cc6a1571a1b
slyfox <slyfox2046@gmail.com>
2019-10-11 16:36:00 +0800

    webpack4 study

--- lesson3-1b/src/index.js
+++ lesson3-1b/src/index.js
@@ -1,10 +1,6 @@
+import avatar from  './avatar.jpg';
 
-var Header =require('./header');
-var Sidebar =require('./sidebar');
-var Content =require('./content');
-var avatar = require('./avatar.jpg')
-console.log(avatar);// 输出文件名 305c986be207750331c4eccc19f875a4.jpg
-
-new Header();
-new Sidebar();
-new Content();
+var img = new Image();
+img.src =avatar;
+var root = document.getElementById("root");
+root.append(img);
