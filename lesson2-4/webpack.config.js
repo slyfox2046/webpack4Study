1ca4e974f22968273bd4c4aab8cbfbe63f8fe1e5
slyfox <slyfox2046@gmail.com>
2019-10-11 15:32:22 +0800

    webpack4 study

--- lesson2-4/webpack.config.js
+++ lesson2-4/webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    entry :"./index.js",
+    mode: "development",// development 开发模式，未被压缩，production 线上模式，被压缩了
+    
+    // entry :"./src/index.js", //入口文件，放在了src目录下
+    entry :{
+        main:"./src/index.js"
+    },
+
     output : {
         filename : 'bundle.js', //输出的文件名
-        path : path.resolve(__dirname,'bundle')  //配置路径
+        path : path.resolve(__dirname,'dist')  //配置路径
     }
 }
