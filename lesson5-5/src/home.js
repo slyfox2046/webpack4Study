3eb67b22a58d9f6d6da7c03285a8fbac2e705d1d
Administrator <slyfox2046@gmail.com>
2019-11-11 08:40:15 +0800

    webpack4Study

---
+++ lesson5-5/src/home.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+import ReactDom from 'react-dom';
+
+class Home extends Component {
+    render(){
+        return <div>HomePage</div>
+    }
+}
+export default Home;
---
+++ lesson5-5/src/index.js
@@ -0,0 +1,26 @@
+// import "@babel/polyfill";   // 不引入，英文.babelrc中已经配置了 "useBuiltIns": "usage" ，会自动加载polyfill
+
+import React,{Component} from 'react';
+import {BrowserRouter,Route } from 'react-router-dom'
+import ReactDom from 'react-dom';
+
+import Home from './home'
+import List from './list'
+
+
+class App extends Component{
+    render() {
+        return(
+
+            <BrowserRouter>
+                <div>
+                    <Route path='/' component ={Home}/>
+                    <Route path='/list' component ={List}/>
+                </div>
+
+            </BrowserRouter>
+        )
+    }
+}
+ReactDom.render(<App />,document.getElementById('root'));
+
