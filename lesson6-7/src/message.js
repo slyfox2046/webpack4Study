c41a65c91f7bb46817c5efcdf4522da928bb327d
slyfox <slyfox2046@gmail.com>
2019-12-21 21:42:20 +0800

    lesson6-7

--- lesson6-7/src/message.js
+++ lesson6-7/src/message.js
@@ -1,9 +1,5 @@
-/*
- * @Author: Admin
- * @Date: 2019-12-21 16:38:32
- * @FilePath: \webpack4Study\lesson6-4\src\message.js
- * @Description: file content
- */
+
+
 import { word } from './word.js';
 const message = `say ${word}`;
 export default message;
