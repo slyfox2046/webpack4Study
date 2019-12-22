341ff0216af19f8f2287b3719309e8f5d1346f70
slyfox <slyfox2046@gmail.com>
2019-12-22 14:28:26 +0800

    lesson7-4

--- lesson7-3/my-project/vue.config.js
+++ lesson7-3/my-project/vue.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
 module.exports = {
-  // outputDir: 'dell',
-  css: {
-    modules:true
-  }
+  outputDir: 'dell',
+  // css: {
+  //   modules:true
+  // }
+
+  // 原生配置，定义到static里找内容
+  // configureWebpack: {
+  //   devServer: { contentBase: [path.resolve(__dirname, 'static')] },
+  // },
+  devServer: { contentBase: [path.resolve(__dirname, 'static')] },
 };
